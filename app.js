83ede7073cc63937758fe6f1307c5656c313aa95
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-10-30 15:30:54 +0900

    feat:express-session

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 
+const session = require("express-session");
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var session_opt = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/hello.js
+++ routes/hello.js
@@ -2,13 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    var name = req.query.name;
-    var mail = req.query.mail;
+    var msg = "何か書いて送信してください";
+    if (req.session.message != undefined) {
+        msg = "Last Message:" + req.session.message;
+    }
     var data = {
         title: "Hello!",
-        content: "あなたの名前は" + name + "<br>" + "メールアドレスは" + mail + "です．"
+        content: msg
     };
     res.render("hello", data);
 });
+
+router.post("/post", (req, res, next) => {
+    var msg = req.body["message"];
+    req.session.message = msg;
+    var data = {
+        title: "Hello!",
+        content: "Last Message:" + req.session.message
+    };
+    res.render("hello", data);
+})
 
 module.exports = router;
