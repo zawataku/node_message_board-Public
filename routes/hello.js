03191c1e771496beefca2162d1342d5cc044496e
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-10-30 16:29:43 +0900

    feat:sqlite3

--- routes/hello.js
+++ routes/hello.js
@@ -1,26 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    var msg = "何か書いて送信してください";
-    if (req.session.message != undefined) {
-        msg = "Last Message:" + req.session.message;
-    }
-    var data = {
-        title: "Hello!",
-        content: msg
-    };
-    res.render("hello", data);
-});
+const sqlite3 = require('sqlite3');
 
-router.post("/post", (req, res, next) => {
-    var msg = req.body["message"];
-    req.session.message = msg;
-    var data = {
-        title: "Hello!",
-        content: "Last Message:" + req.session.message
-    };
-    res.render("hello", data);
-})
+const db = new sqlite3.Database('./db/mydata.db');
+
+router.get('/', (req, res, next) => {
+    db.serialize(() => {
+        var rows = "";
+        db.each("select * from mydata", (err, row) => {
+            if (!err) {
+                rows += "<tr><th>" + row.id + "</th><td>" + row.name + "</td></tr>";
+            }
+        }, (err, count) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello!',
+                    content: rows
+                };
+                res.render('hello', data);
+            }
+        });
+    });
+});
 
 module.exports = router;
