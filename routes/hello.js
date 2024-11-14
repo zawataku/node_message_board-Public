0bdf3d665d27c022270b14e08d0fc56946748989
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-14 14:14:45 +0900

    update:hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -38,7 +38,10 @@ router.get('/add', (req, res, next) => {
 router.post('/add', [
     check('name', 'NAME は必ず入力して下さい。').notEmpty(),
     check('mail', 'MAIL はメールアドレスを記入して下さい。').isEmail(),
-    check('age', 'AGE は年齢（整数）を入力下さい。').isInt()
+    check('age', 'AGE は年齢（整数）を入力下さい。').isInt(),
+    check('age', 'AGE は0以上120以下で入力下さい。').custom(value => {
+        return value >= 0 && value <= 120;
+    })
 ], (req, res, next) => {
     const errors = validationResult(req);
 
