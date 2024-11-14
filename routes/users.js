0fbf4e6792ff9c704405cfd470283d0b1bcc2903
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-14 17:09:01 +0900

    feat:deleteページ

--- routes/users.js
+++ routes/users.js
@@ -95,5 +95,25 @@ router.post('/edit', (req, res, next) => {
   });
 });
 
+router.get('/delete/:id', (req, res, next) => {
+  const id = req.params.id;
+  prisma.user.findUnique(
+    { where: { id: +id } }
+  ).then(usr => {
+    const data = {
+      title: 'Users/Delete',
+      user: usr
+    };
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete', (req, res, next) => {
+  prisma.User.delete({
+    where: { id: +req.body.id }
+  }).then(() => {
+    res.redirect('/users');
+  });
+});
 
 module.exports = router;
