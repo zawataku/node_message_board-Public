f654dcd2f1abd19f4beff0b33c8c7408bd25b6c3
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-14 13:24:12 +0900

    feat:レコードを表示するページ

--- routes/hello.js
+++ routes/hello.js
@@ -18,7 +18,43 @@ router.get('/', (req, res, next) => {
                     title: 'Hello!',
                     content: rows
                 };
-                res.render('hello', data);
+                res.render('hello/index', data);
+            }
+        });
+    });
+});
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/Add',
+        content: '新しいレコードを入力：'
+    }
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    db.serialize(() => {
+        db.run('insert into mydata (name, mail, age) values (?, ?, ?)',
+            nm, ml, ag);
+    });
+    res.redirect('/hello');
+});
+
+router.get('/show', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/show',
+                    content: 'id = ' + id + ' のレコード：',
+                    mydata: row
+                }
+                res.render('hello/show', data);
             }
         });
     });
