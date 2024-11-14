ee239214a47599aaef7c56d9e6b8f26a8cf500bf
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-14 14:08:15 +0900

    feat:findページ

--- routes/hello.js
+++ routes/hello.js
@@ -115,4 +115,38 @@ router.post('/delete', (req, res, next) => {
     res.redirect('/hello');
 });
 
+router.get('/find', (req, res, next) => {
+    db.serialize(() => {
+        db.all("select * from mydata", (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/find',
+                    find: '',
+                    content: '検索条件を入力して下さい。',
+                    mydata: rows
+                };
+                res.render('hello/find', data);
+            }
+        });
+    });
+});
+
+router.post('/find', (req, res, next) => {
+    var find = req.body.find;
+    db.serialize(() => {
+        var q = "select * from mydata where ";
+        db.all(q + find, [], (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/find',
+                    find: find,
+                    content: '検索条件 ' + find,
+                    mydata: rows
+                }
+                res.render('hello/find', data);
+            }
+        });
+    });
+});
+
 module.exports = router;
