07b22a3ff6e87cfab0b6055539781f6ca313b898
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-14 13:52:06 +0900

    feat:新ページ

--- routes/hello.js
+++ routes/hello.js
@@ -60,4 +60,59 @@ router.get('/show', (req, res, next) => {
     });
 });
 
+router.get('/edit', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/edit',
+                    content: 'id = ' + id + ' のレコードを編集：',
+                    mydata: row
+                }
+                res.render('hello/edit', data);
+            }
+        });
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+    const id = req.body.id;
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    const q = "update mydata set name = ?, mail = ?, age = ? where id = ?";
+    db.serialize(() => {
+        db.run(q, nm, ml, ag, id);
+    });
+    res.redirect('/hello');
+});
+
+router.get('/delete', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Delete',
+                    content: 'id = ' + id + ' のレコードを削除：',
+                    mydata: row
+                }
+                res.render('hello/delete', data);
+            }
+        });
+    });
+});
+
+router.post('/delete', (req, res, next) => {
+    const id = req.body.id;
+    db.serialize(() => {
+        const q = "delete from mydata where id = ?";
+        db.run(q, id);
+    });
+    res.redirect('/hello');
+});
+
 module.exports = router;
